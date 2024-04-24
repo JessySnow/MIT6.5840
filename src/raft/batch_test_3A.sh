5b909acc2cebbb58bb5fae08b00f0fea2ebd4c28
JessySnow <jessysnow12345@gmail.com>
2024-04-25 07:12:38 +0800

    refactor: 修改Lab3A测试脚本

--- src/raft/batch_test_3A.sh
+++ src/raft/batch_test_3A.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for i in $(seq 1 50); do
+for _ in $(seq 1 50); do
   go test -run 3A >> a.txt
 done
 
 if grep -q FAIL a.txt; then
   echo "FAIL"
+  exit 1
 else
   echo "PASS"
+  exit 0
 fi
